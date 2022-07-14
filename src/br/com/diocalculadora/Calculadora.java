6848c54ac059df4a13918aa231f8c174702973d2
Mariana Lima <marianalima.amb@gmail.com>
2022-07-14 16:59:24 -0300

    creates calculadora

---
+++ src/br/com/diocalculadora/Calculadora.java
@@ -0,0 +1,47 @@
+package br.com.diocalculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a= scan.nextInt();
+		System.out.println("Digite o segundo valor");
+		b= scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		int divisao = divisao(a,b);
+		
+		System.out.println("A soma é: " +soma);
+		System.out.println("A subtração é: " + subtracao);
+		System.out.println("A multiplicação é: " + multiplicacao);
+		System.out.println("A divisão é: " + divisao);
+	}
+
+
+ public static int soma(int a, int b) {
+	return a+b;
+ }
+ 
+ public static int subtracao(int a, int b) {
+	return a-b;
+ }
+ 
+ public static int multiplicacao(int a, int b) {
+	 return a*b;
+ }
+ 
+ public static int divisao(int a, int b) {
+	 return a/b;
+ }
+ 
+ 
+ 
+
+}
